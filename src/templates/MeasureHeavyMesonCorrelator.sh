8a0314e8dcf6673243998277f5a06d939f3b2040
Alexander Lehmann <alexander.lehmann@uis.no>
2019-03-21 19:47:10 +0100

    Template script 'MeasureHeavyMesonCorrelator.sh' updated

--- src/templates/MeasureHeavyMesonCorrelator.sh
+++ src/templates/MeasureHeavyMesonCorrelator.sh
@@ -4,15 +4,15 @@ processes=4
 exename=./run.exe
 
 mode=3
-Nx=12
-Ny=12
-Nz=12
+Nx=16
+Ny=16
+Nz=16
 ax=1.0
 ay=1.0
 az=1.0
 
 CoMTime=0.
-TimeRange=1.
+TimeRange=10.
 TimeSpacing=0.1
 
 # Initialisation of gluon field
@@ -20,8 +20,9 @@ RandomSeed=1
 # Given in units of az
 InitialGluonSaturationScale=1.
 InitialGluonOccupationAmplitude=1.
-Coupling=0.0
+Coupling=0.1
 # Quarks
+iterativeTol=1.E-10
 Mass=5.
 c0_re=1. ; c0_im=0.
 c1_re=1. ; c1_im=0.
@@ -31,4 +32,4 @@ c3_re=1. ; c3_im=0.
 
 mpiexec -n $processes \
 	$exename \
-	$mode $Nx $Ny $Nz $TimeSpacing $ax $ay $az $CoMTime $TimeRange $RandomSeed $InitialGluonSaturationScale $InitialGluonOccupationAmplitude $Coupling $Mass $c0_re $c0_im $c1_re $c1_im $c2_re $c2_im $c3_re $c3_im
+	$mode $Nx $Ny $Nz $TimeSpacing $ax $ay $az $CoMTime $TimeRange $RandomSeed $InitialGluonSaturationScale $InitialGluonOccupationAmplitude $Coupling $iterativeTol $Mass $c0_re $c0_im $c1_re $c1_im $c2_re $c2_im $c3_re $c3_im
